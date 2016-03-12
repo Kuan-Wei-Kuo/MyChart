9a17bc724e2deb6987252816e9ea6c2e45fa8452
Kuan-Wei Kuo <k.kuanwei@gmail.com>
2016-03-12 13:34:35 +0800

    Add Zoom and Scroll Animator

--- app/src/main/java/com/kuo/mychart/view/ViewAnamatorTest.java
+++ app/src/main/java/com/kuo/mychart/view/ViewAnamatorTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -19,6 +20,8 @@ public class ViewAnamatorTest extends View {
 
     private Paint axisPaint;
     private float padding = 50;
+    private float textWidth;
+    private float textTotalWidth;
 
     public ViewAnamatorTest(Context context) {
         super(context);
@@ -51,25 +54,60 @@ public class ViewAnamatorTest extends View {
         super.onDraw(canvas);
 
         drawAxisX(canvas);
+        drawGraph(canvas);
     }
+
+    private float scrollX = 0;
 
     private void drawAxisX(Canvas canvas) {
 
         String[] axisXs = {"01/01", "01/02", "01/03", "01/04", "01/05", "01/06", "01/07", "01/08"};
         int count = 0;
 
+        textTotalWidth = (textWidth + padding) * axisXs.length;
+
+        Rect textBounds = new Rect();
+        axisPaint.getTextBounds(axisXs[0], 0, axisXs[0].length(), textBounds);
+        textWidth = textBounds.right - textBounds.left;
+
         for(String string : axisXs) {
 
-            Rect textBounds = new Rect();
             axisPaint.getTextBounds(string, 0, string.length(), textBounds);
             int textHeight = textBounds.bottom - textBounds.top;
             int textWidth = textBounds.right - textBounds.left;
 
-            canvas.drawText(string, count * (textWidth + padding), textHeight / 2 + padding, axisPaint);
+            canvas.drawText(string, count * (textWidth + padding) + scrollX, textHeight / 2 + 100, axisPaint);
 
             count++;
         }
 
+    }
+
+    private void drawGraph(Canvas canvas) {
+
+        String[] axisXs = {"01/01", "01/02", "01/03", "01/04", "01/05", "01/06", "01/07", "01/08"};
+        int count = 0;
+
+        Rect textBounds = new Rect();
+        axisPaint.setColor(ChartRendererUntil.CHART_GREEN);
+
+        for(String string : axisXs) {
+
+            axisPaint.getTextBounds(string, 0, string.length(), textBounds);
+            int textHeight = textBounds.bottom - textBounds.top;
+            int textWidth = textBounds.right - textBounds.left;
+
+            float left = count * (textWidth + padding) + scrollX;
+            float top = 150;
+            float right = left + textWidth / 2;
+            float bottom = 250;
+
+            RectF rectF = new RectF(left, top, right, bottom);
+
+            canvas.drawRect(rectF, axisPaint);
+
+            count++;
+        }
     }
 
     private float downX, downY, dX;
@@ -83,19 +121,37 @@ public class ViewAnamatorTest extends View {
                 downX = event.getX();
                 downY = event.getY();
 
-                dX = padding - event.getX();
+                dX = scrollX - event.getX();
 
                 break;
             case MotionEvent.ACTION_MOVE:
 
-                padding = event.getX() + dX;
-
-                Log.d("padding", padding + "");
-
+                scroll(event, dX);
+                //zoom(event, dX);
                 break;
         }
 
         invalidate();
         return true;
+    }
+
+    private void scroll(MotionEvent event, float dX) {
+        scrollX = event.getX() + dX;
+
+        if(textTotalWidth + scrollX <= getWidth()) {
+            scrollX = getWidth() - textTotalWidth;
+        } else if(scrollX >= 0) {
+            scrollX = 0;
+        }
+
+        Log.d("scrollX", scrollX + "");
+    }
+
+    private void zoom(MotionEvent event, float dX) {
+        padding = event.getX() + dX;
+
+        if(padding <= textWidth) {
+            padding = textWidth;
+        }
     }
 }
